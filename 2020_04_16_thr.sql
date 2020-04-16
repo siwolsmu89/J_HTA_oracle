b88dd57d0e6ed7f9cb2e7abaca41ac7e13803c7f
s_smu <siwol_smuire89@naver.com>
2020-04-16 12:39:48 +0900

    2020/04/16

--- 2020_04_16_thr.sql
+++ 2020_04_16_thr.sql
@@ -93,3 +93,38 @@ where manager_id is not null;
 select employee_id, first_name, job_id, salary
 from employees
 where salary between 2000 and 3000;
+
+-- In(값1, 값2, 값3...) 사용하기
+-- 10번 부서와 20번 부서에 소속된 사원의 아이디, 이름, 소속부서 아이디 조회하기
+select employee_id, first_name, department_id
+from employees
+where department_id in (10, 20);
+
+-- 50, 60, 70, 80번 부서에 소속되지 않은 사원의 아이디, 이름, 소속부서 아이디 조회하기
+select employee_id, first_name, department_id
+from employees
+where department_id not in (50, 60, 70, 80);
+
+-- 국가아이디가 JP, US인 지역의 위치아이디, 주소, 국가아이디 조회하기
+select location_id, street_address, country_id
+from locations
+where country_id in ('JP', 'US');
+
+-- 직원의 직종아이디가 AD_PRES이거나 AD_VP인 직원의 아이디, 이름, 직종을 조회하기
+SELECT EMPLOYEE_ID, FIRST_NAME, JOB_ID
+FROM EMPLOYEES
+WHERE JOB_ID IN('AD_PRES', 'AD_VP');
+
+-- 50번 혹은 60번 부서에 근무한 적이 있는 사원의 아이디, 근무시작일, 근무종료일을 조회하기
+SELECT employee_id, start_date, end_date
+from job_history
+where department_id in (50, 60);
+
+-- 101번이나 102번 직원이 상사로 지정된 사원의 아이디, 이름, 매니저아이디를 조회하기
+select employee_id, first_name, manager_id
+from employees
+where manager_id in(101,102);
+
+select first_name
+from employees
+where first_name like '___________';
