974582375eb7c68310837c60f7221616a940d720
s_smu <siwol_smuire89@naver.com>
2020-04-20 16:15:39 +0900

    2020/04/20

--- 2020_04_20_mon.sql
+++ 2020_04_20_mon.sql
@@ -106,8 +106,58 @@ order by D.department_id;
 -- Seattle에서 근무하는 직원의 아이디, 이름, 직종, 소속부서 아이디, 소속부서명을 조회하기
 select E.employee_id, E.first_name, E.job_id
     , D.department_id, D.department_name
-from employees E, departments D, locations L
-where E.department_id = D.department_id
-    and D.location_id = L.location_id
-    and L.city = 'Seattle'
+from locations L, departments D, employees E
+where L.city = 'Seattle'
+    and L.location_id = D.location_id
+    and D.department_id = E.department_id
 order by D.department_id, E.employee_id;
+
+-- 6교시
+
+-- 직종 변경 이력에서
+-- 직원아이디, 직원이름, 업무시작일, 업무종료일, 직종아이디, 직종제목, 소속부서아이디, 부서명 조회하기
+select H.employee_id, E.first_name
+    , H.start_date, H.end_date
+    , H.job_id, J.job_title
+    , H.department_id, D.department_name
+from job_history H, employees E, jobs J, departments D
+where H.employee_id = E.employee_id
+    and H.job_id = J.job_id
+    and H.department_id = D.department_id
+order by H.employee_id, H.start_date;
+
+create table job_grades(
+    gra char(1) primary key, 
+    lowest_sal number(6,0) not null,
+    highest_sal number(6,0) not null
+);
+
+insert into job_grades values('A', 1000, 2999);
+insert into job_grades values('B', 3000, 5999);
+insert into job_grades values('C', 6000, 9999);
+insert into job_grades values('D', 10000, 14999);
+insert into job_grades values('E', 15000, 24999);
+insert into job_grades values('F', 25000, 400000);
+
+commit;
+
+-- 7교시
+
+select E.employee_id, E.first_name, E.salary
+    , G.gra as grade
+from employees E, job_grades G
+where E.salary >= G.lowest_sal 
+    and E.salary <= G.highest_sal
+order by salary desc;
+
+-- 비등가 조인
+-- 50번 부서에 소속된 직원의 급여 등급을 조회하기
+-- 직원아이디, 이름, 급여, 급여 등급
+select E.employee_id, E.first_name
+    , G.lowest_sal min, E.salary, G.highest_sal max
+    , G.gra as grade
+from employees E, job_grades G
+where E.department_id = 50
+    and E.salary >= G.lowest_sal
+    and E.salary <= G.highest_sal
+order by salary desc, first_name;
