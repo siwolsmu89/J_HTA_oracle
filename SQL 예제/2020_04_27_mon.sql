d0b216e2139cf0e61b9fafceb0185b6a268779fb
s_smu <siwol_smuire89@naver.com>
2020-04-27 15:53:45 +0900

    2020/04/27

--- SQL 예제/2020_04_27_mon.sql
+++ SQL 예제/2020_04_27_mon.sql
@@ -32,3 +32,31 @@ ROLLBACK; -- 새로 시작된 트랜잭션의 변경사항(DML1~3)을 버림, 현재 트랜잭션 종료,
 
 SELECT ROWID, user_id, user_email
 FROM sample_book_users;
+
+-- 북스토어 문의 게시판
+-- 번호,    제목,     작성자,    내용,     조회수, 작성일, 상태(답변완료여부), 질문유형
+-- NUMBER, VARCHAR2, VARCHAR2, VARCHAR2, NUMBER, DATE, CHAR,             VARCHAR2
+CREATE TABLE sample_book_questions (
+    question_no                    NUMBER(7,0)      PRIMARY KEY, 
+    question_title                 VARCHAR2(500)    NOT NULL, 
+    user_id                        VARCHAR2(50)     REFERENCES sample_book_users (user_id), 
+    question_content               VARCHAR2(4000)   NOT NULL,
+    question_view_count            NUMBER(7,0)      DEFAULT 0,
+    question_registered_date       DATE             DEFAULT SYSDATE,
+    question_status                CHAR(1)          DEFAULT 'N',
+    question_type                  VARCHAR2(200)    NOT NULL
+);
+
+-- 북스토어 답변 게시판
+-- 번호,    내용,     작성일, 문의글번호
+-- NUMBER, VARCHAR2, DATE,  NUMBER
+CREATE TABLE sample_book_answers (
+    answer_no                      NUMBER(7,0)     PRIMARY KEY,
+    answer_content                 VARCHAR2(4000)  NOT NULL,
+    answer_registered_date         DATE            DEFAULT SYSDATE,
+    question_no                    NUMBER(7,0)     REFERENCES sample_book_questions (question_no)
+);
+
+-- 테이블 이름 변경하기
+-- RENAME 지금 이름 TO 새 이름
+RENAME sample_books_answers TO sample_book_answers;
