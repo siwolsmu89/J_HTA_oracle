c423bdf777472fd828affc3d4fa5f8c8a4f203b3
s_smu <siwol_smuire89@naver.com>
2020-04-28 17:49:31 +0900

    2020/04/28

--- SQL 예제/2020_04_28_tue.sql
+++ SQL 예제/2020_04_28_tue.sql
@@ -32,9 +32,16 @@ VALUES
 (sample_review_seq.NEXTVAL, '오랜만에 꿈도 꾸지 않고 잘 잤습니다', 2, 11021, 'kimkim');
 
 -- 평점평균 확인
-SELECT TRUNC(AVG(review_point),1)
+SELECT TRUNC(AVG(review_point),1) as avg_point
 FROM sample_book_reviews
-WHERE book_no = 10021;
+WHERE book_no = 10011;
+
+UPDATE sample_books
+    SET 
+        book_point = (SELECT TRUNC(AVG(review_point),1)
+                        FROM sample_book_reviews
+                        WHERE book_no = 10011)
+    WHERE book_no = 10011;
 
 -- 추천도서 테이블
 -- 누가 무슨 책을 좋아요 했는지 확인
@@ -47,3 +54,60 @@ CREATE TABLE sample_book_likes (
         REFERENCES sample_book_users (user_id),
     CONSTRAINT likes_uk UNIQUE (book_no, user_id)
 );
+
+CREATE OR REPLACE VIEW emp_salary_grade_view
+		AS SELECT E.employee_id, E.first_name, E.salary, G.gra
+		   FROM employees E, job_grades G
+		   WHERE (E.salary BETWEEN G.lowest_sal AND G.highest_sal);
+    
+-- 뷰를 이용해서 간편하게 원하는 데이터를 조회하기
+-- SELECT문 작성 시 테이블과 똑같은 방법으로 사용할 수 있지만, 물리적으로 데이터가 저장된 것은 아니다.
+-- 주로 복잡한 질의문을 간단하게 사용하기 위해 사용한다.
+-- 뷰를 사용해 INSERT, UPDATE, DELETE 작업은 하지 않는다. (단일 뷰는 가능하지만 X, 복합 뷰는 애초에 INSERT, UPDATE, DELETE가 불가능)
+SELECT *        
+FROM emp_salary_grade_view
+WHERE gra = 'A';
+
+CREATE OR REPLACE VIEW emp_salary_view
+		AS SELECT employee_id, first_name, salary,
+			  salary*4*12 + salary*nvl(commission_pct, 0)*4*12 AS annual_salary
+		   FROM employees;
+           
+-- 인라인뷰 사용하기
+-- 엄청 많이 사용된다.
+SELECT id, name, salary
+FROM (SELECT employee_id AS id, first_name || ' ' || last_name AS name, salary, department_id AS deptid 
+      FROM employees)
+WHERE deptid = 60;
+
+-- 전체 사원들 중에서 자신이 소속된 부서의 평균급여보다 급여를 적게 받는 사원의
+-- 사원 아이디, 이름, 급여, 부서아이디, 부서의 평균급여를 조회하기
+SELECT E.employee_id, E.first_name, E.salary, D.dept_id, D.avg_sal
+FROM employees E, (SELECT department_id AS dept_id, TRUNC(AVG(salary)) AS avg_sal
+                   FROM employees
+                   WHERE department_id IS NOT NULL
+                   GROUP BY department_id) D
+WHERE E.department_id = D.dept_id
+    AND E.salary < D.avg_sal
+ORDER BY E.employee_id;
+
+-- 부서아이디, 부서명, 부서별 사원수, 도시명을 출력하기
+SELECT D.department_id, D.department_name, N.dept_people, L.city
+FROM departments D, locations L, (SELECT department_id, COUNT(*) AS dept_people
+                                  FROM employees
+                                  GROUP BY department_id
+                                  ) N
+WHERE D.location_id = L.location_id
+    AND N.department_id = D.department_id
+ORDER BY N.dept_people DESC, L.city, D.department_name;
+
+
+
+SELECT review_no
+						, review_content 
+						, review_point 
+						, review_registered_date 
+						, book_no 
+						, user_id 
+					FROM sample_book_reviews 
+					WHERE user_id = 'kimmi';
